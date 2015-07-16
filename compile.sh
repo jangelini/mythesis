e42a9c850ef0bbb8926097ae48938302b5e37682
Darien Miranda <panzerfausten@gmail.com>
2015-07-16 15:02:15 -0700

    clean clean clean

--- compile.sh
+++ compile.sh
@@ -1,13 +1,10 @@
+kill %
 rm *.aux
-rm thesis.pdf
-pdflatex thesis
-bibtex thesis
-pdflatex thesis
-pdflatex thesis
-$linux="Linux"
-$os=$(uname)
-if [ "$os" == "$linux" ] ;
-	then okular thesis.pdf;
-else 
-	open thesis.pdf;
-fi
+rm iTesis.pdf
+pdflatex iTesis
+bibtex iTesis
+pdflatex iTesis
+pdflatex iTesis
+
+pdflatex iTesis.tex
+okular iTesis.pdf
